64e17c0d69b5297525470c1385a65b9a1f234cec
Yonatan Ellman <yonatan@nodeside.com>
2014-07-24 17:52:54 +0300

    coorect path for bower

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -2,7 +2,7 @@ var spawn = require('child_process').spawn;
 
 // install bower
 console.log('Installing Bower packages');
-bowered  = spawn('node', ['node_modules/bower/bin/bower', 'install', '--config.interactive=false']);
+bowered  = spawn('node', ['../node_modules/bower/bin/bower', 'install', '--config.interactive=false']);
 bowered.stdin.end();
 
 require('mean-health').emit('install');
