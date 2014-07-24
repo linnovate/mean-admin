29fcf5d3d257635f8b1635cc6df212c12c325d77
Yonatan Ellman <yonatan@nodeside.com>
2014-07-24 18:06:08 +0300

    removing post install

--- bin/postinstall.js
+++
@@ -1,9 +0,0 @@
-var spawn = require('child_process').spawn;
-
-// install bower
-console.log('Installing Bower packages');
-bowered  = spawn('node', ['../node_modules/bower/bin/bower', 'install', '--config.interactive=false']);
-bowered.stdin.end();
-
-// Publish that the package has been installed
-require('mean-health').emit('install');
