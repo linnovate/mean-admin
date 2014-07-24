2126bc82dbac9e80579bfa37fcff96929a51df84
Yonatan Ellman <yonatan@nodeside.com>
2014-07-24 17:49:48 +0300

    post install to use mean-health

---
+++ bin/postinstall.js
@@ -0,0 +1,8 @@
+var spawn = require('child_process').spawn;
+
+// install bower
+console.log('Installing Bower packages');
+bowered  = spawn('node', ['node_modules/bower/bin/bower', 'install', '--config.interactive=false']);
+bowered.stdin.end();
+
+require('mean-health').emit('install');
